4fe21b485c313296b8f46789b672dd96b9606852
Eung Jin Lee <oiojin831@gmail.com>
2021-06-18 11:14:00 +0900

    fix bug

--- index.py
+++ index.py
@@ -1,14 +1,14 @@
 #!/usr/bin/env python
 # coding: utf-8
 
-# In[1]:
+# In[2]:
 
 
 from bs4 import BeautifulSoup
 from urllib.request import urlopen
 
 
-# In[2]:
+# In[3]:
 
 
 url = "https://movie.naver.com/movie/running/premovie.nhn"
@@ -16,15 +16,15 @@ page = urlopen(url)
 soup = BeautifulSoup(page, 'lxml')
 
 
-# In[44]:
+# In[4]:
 
 
-soup_by_date = soup.find_all("div", class_="lst_wrap")[1]
-date_all_list = soup_by_date.ul.find_all("li")
+soup_by_date = soup.find_all("div", class_="lst_wrap")
+date_all_list = soup_by_date[1].ul.find_all("li")
 date_all_list
 
 
-# In[29]:
+# In[5]:
 
 
 date_all_list[1].find("dt", class_="tit").a.text
@@ -33,26 +33,22 @@ for li in date_all_list:
     print(li.find("dt", class_="tit").a.text)
 
 
-# In[42]:
+# In[8]:
 
 
 daily_movies = []
 for by_date in soup_by_date:
-    
-
-
-# In[ ]:
-
-
-daily_movies = []
-for by_date in soup_by_date:
-    if by_date is not None:
-        all_list = by_date.ul.find_all("li")
-        continue
+    all_list = by_date.ul.find_all("li")
     titles = []
     for list in all_list:
         titles.append(list.find("dt", class_="tit").a.text)
     daily_movies.append(titles)
 
 daily_movies
+
+
+# In[ ]:
+
+
+
 
